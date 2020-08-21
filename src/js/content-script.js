953dcb50fe225a70c3ca1f3b3b611dd2ff84d242
YingRui Lu <yingruilu@zhulux.com>
2020-08-21 22:15:53 +0800

    update wexin110 redirect strategy and remove jquery

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,7 +1,13 @@
-﻿$(() => {
-  const reg = new RegExp('(^|&)url=([^&]*)(&|$)'); // 构造一个含有目标参数的正则表达式对象
-  const r = window.location.search.substr(1).match(reg); // 匹配目标参数
-  if (r != null) {
-    window.location.href = unescape(r[2]);
-  }
-});
+﻿const host = window.location.origin;
+let jumpToHref = undefined;
+
+if (host === 'https://weixin110.qq.com') {
+  jumpToHref = document.getElementsByTagName('a')[0].href;
+} else if (host === 'https://support.weixin.qq.com') {
+  let url = document.body.getAttribute('url');
+  jumpToHref = url.replace(/&amp;/g, '&');
+}
+
+if (jumpToHref !== undefined) {
+  window.location.href = jumpToHref;
+}
