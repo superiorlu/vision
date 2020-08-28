c0b09204142cefa8efb23a8b2d706601c46e0d5f
YingRui Lu <yingruilu@zhulux.com>
2020-08-28 23:42:16 +0800

    improve fetch jump link

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,11 +1,15 @@
 ﻿const host = window.location.origin;
 let jumpToHref = undefined;
 
-if (host === 'https://weixin110.qq.com') {
-  jumpToHref = document.getElementsByTagName('a')[0].href;
-} else if (host === 'https://support.weixin.qq.com') {
-  let url = document.body.getAttribute('url');
-  jumpToHref = url.replace(/&amp;/g, '&');
+try {
+  if (host === 'https://weixin110.qq.com') {
+    jumpToHref = document.getElementsByClassName('weui-btn')[0].href;
+  } else if (host === 'https://support.weixin.qq.com') {
+    let url = document.body.getAttribute('url');
+    jumpToHref = url.replace(/&amp;/g, '&');
+  }
+} catch (e) {
+  console.log(`[vision] has an error: ${e}`);
 }
 
 if (jumpToHref !== undefined) {
